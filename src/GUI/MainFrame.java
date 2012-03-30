9d3e9b4003ebab8403ce3a31d13e0260099b4454
Gustavo Adolfo Rodriguez <gustalibreros@gmail.com>
2012-03-29 23:46:17 -0500

    Update

--- src/GUI/MainFrame.java
+++ src/GUI/MainFrame.java
@@ -50,6 +50,7 @@ public class MainFrame extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        new GestionEstudiantes().setVisible(true);
     }//GEN-LAST:event_jButton1ActionPerformed
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
